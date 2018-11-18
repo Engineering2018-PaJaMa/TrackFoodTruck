3b6138a1de159e8ae0a11893306e8216953f647b
moody16 <jan.bargiel@gmail.com>
2018-11-18 12:52:55 +0100

    Code clean up

--- app/src/main/java/com/example/pajama/trackfoodtruck/Activities/RegisterActivity.java
+++ app/src/main/java/com/example/pajama/trackfoodtruck/Activities/RegisterActivity.java
@@ -1,13 +1,10 @@
 package com.example.pajama.trackfoodtruck.Activities;
 
-import java.util.Objects;
-
 import com.example.pajama.trackfoodtruck.R;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 import android.view.View;
 
 public class RegisterActivity extends AppCompatActivity
@@ -18,10 +15,6 @@ public class RegisterActivity extends AppCompatActivity
 	{
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_register);
-
-		Toolbar myToolbar = findViewById(R.id.ActivityToolbar);
-		setSupportActionBar(myToolbar);
-		Objects.requireNonNull(getSupportActionBar()).setDisplayShowTitleEnabled(false);
 	}
 
 	public void registerUser(View view)
