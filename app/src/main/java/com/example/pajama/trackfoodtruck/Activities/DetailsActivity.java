74d7a1fa1e739e2361704205d08da684815b4f84
moody16 <jan.bargiel@gmail.com>
2018-11-21 14:29:56 +0100

    Complete details activity.

--- app/src/main/java/com/example/pajama/trackfoodtruck/Activities/DetailsActivity.java
+++ app/src/main/java/com/example/pajama/trackfoodtruck/Activities/DetailsActivity.java
@@ -31,7 +31,7 @@ public class DetailsActivity extends AppCompatActivity
 	protected void onCreate(Bundle savedInstanceState)
 	{
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main);
+		setContentView(R.layout.activity_details);
 
 		ReviewListAdapter reviewListAdapter = new ReviewListAdapter(this, dateArray, authorArray, reviewArray);
 		listView = (ListView) findViewById(R.id.reviewsList);
--- app/src/main/java/com/example/pajama/trackfoodtruck/Fragments/FavouriteFragment.java
+++ app/src/main/java/com/example/pajama/trackfoodtruck/Fragments/FavouriteFragment.java
@@ -1,15 +1,18 @@
 package com.example.pajama.trackfoodtruck.Fragments;
 
+import com.example.pajama.trackfoodtruck.Activities.DetailsActivity;
 import com.example.pajama.trackfoodtruck.ListAdapter.FavouriteFoodTruckListAdapter;
 import com.example.pajama.trackfoodtruck.R;
 
 import android.content.Context;
+import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.ListView;
 
 public class FavouriteFragment extends Fragment
@@ -27,6 +30,7 @@ public class FavouriteFragment extends Fragment
 	public void onCreate(Bundle savedInstanceState)
 	{
 		super.onCreate(savedInstanceState);
+
 	}
 
 	@Override
@@ -37,6 +41,26 @@ public class FavouriteFragment extends Fragment
 
 		listView = view.findViewById(R.id.favouriteListView);
 		listView.setAdapter(favouriteFoodTruckListAdapter);
+
+		listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+
+			@Override
+			public void onItemClick(AdapterView<?> arg0, View arg1,
+					int position, long arg3) {
+				// TODO Auto-generated method stub
+
+
+				Intent intent = new Intent(getActivity(), DetailsActivity.class);
+				startActivity(intent);
+
+
+			}
+		});
+
 		return view;
 	}
+
+
+
+
 }
