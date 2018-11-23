384fdbbeba7f23fd54e2f278360f01ed09dc178e
moody16 <jan.bargiel@gmail.com>
2018-11-23 11:37:11 +0100

    Add toolbar to details Activity

--- app/src/main/java/com/example/pajama/trackfoodtruck/Activities/DetailsActivity.java
+++ app/src/main/java/com/example/pajama/trackfoodtruck/Activities/DetailsActivity.java
@@ -1,10 +1,17 @@
 package com.example.pajama.trackfoodtruck.Activities;
 
+import java.util.Objects;
+
 import com.example.pajama.trackfoodtruck.ListAdapter.ReviewListAdapter;
 import com.example.pajama.trackfoodtruck.R;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.widget.ListView;
 
 public class DetailsActivity extends AppCompatActivity
@@ -33,9 +40,37 @@ public class DetailsActivity extends AppCompatActivity
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_details);
 
+		Toolbar myToolbar = findViewById(R.id.ActivityToolbar);
+		setSupportActionBar(myToolbar);
+		Objects.requireNonNull(getSupportActionBar()).setDisplayShowTitleEnabled(false);
+
 		ReviewListAdapter reviewListAdapter = new ReviewListAdapter(this, dateArray, authorArray, reviewArray);
 		listView = (ListView) findViewById(R.id.reviewsList);
 		listView.setAdapter(reviewListAdapter);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu)
+	{
+		MenuInflater menuInflater = getMenuInflater();
+		menuInflater.inflate(R.menu.menu, menu);
+		return super.onCreateOptionsMenu(menu);
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		switch (item.getItemId()) {
+			case R.id.action_settings:
+
+				Intent intent = new Intent(DetailsActivity.this, SettingsActivity.class);
+				startActivity(intent);
+				return true;
+
+			default:
+
+				return super.onOptionsItemSelected(item);
+
+		}
 	}
 
 }
