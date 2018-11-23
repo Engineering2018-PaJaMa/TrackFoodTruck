ee371b7ca21f21ea842fa87680eb6eb509bb49f5
moody16 <jan.bargiel@gmail.com>
2018-11-23 12:25:10 +0100

    Add button to create opinion

--- app/src/main/java/com/example/pajama/trackfoodtruck/Activities/DetailsActivity.java
+++ app/src/main/java/com/example/pajama/trackfoodtruck/Activities/DetailsActivity.java
@@ -5,15 +5,22 @@ import java.util.Objects;
 import com.example.pajama.trackfoodtruck.ListAdapter.ReviewListAdapter;
 import com.example.pajama.trackfoodtruck.R;
 
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.text.InputType;
+import android.util.TypedValue;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ListView;
+import android.widget.TextView;
 
 public class DetailsActivity extends AppCompatActivity
 {
@@ -57,6 +64,16 @@ public class DetailsActivity extends AppCompatActivity
 		ReviewListAdapter reviewListAdapter = new ReviewListAdapter(this, dateArray, authorArray, reviewArray);
 		listView = (ListView) findViewById(R.id.reviewsList);
 		listView.setAdapter(reviewListAdapter);
+
+		Button clickButton = (Button) findViewById(R.id.addOpinionButton);
+		clickButton.setOnClickListener( new View.OnClickListener() {
+
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+                addOpinion();
+			}
+		});
 	}
 
 	@Override
@@ -81,6 +98,36 @@ public class DetailsActivity extends AppCompatActivity
 				return super.onOptionsItemSelected(item);
 
 		}
+	}
+
+
+
+	public void addOpinion() {
+
+		AlertDialog.Builder builder = new AlertDialog.Builder(this);
+		builder.setTitle("Add Opinion");
+
+		// Set up the input
+		final EditText input = new EditText(this);
+		// Specify the type of input expected; this, for example, sets the input as a password, and will mask the text
+		input.setInputType(InputType.TYPE_CLASS_TEXT | InputType.TYPE_CLASS_TEXT);
+		builder.setView(input);
+
+		// Set up the buttons
+		builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+			@Override
+			public void onClick(DialogInterface dialog, int which) {
+				String m_Text = input.getText().toString();
+			}
+		});
+		builder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+			@Override
+			public void onClick(DialogInterface dialog, int which) {
+				dialog.cancel();
+			}
+		});
+
+		builder.show();
 	}
 
 }
